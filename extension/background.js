49a2254df5d6e5fec1d09377870f1913d8c7dd5c
rjabdurrahman <md.rahman0296@gmail.com>
2019-06-20 17:39:24 +0600

    Added: Run icon

--- extension/background.js
+++ extension/background.js
@@ -11,9 +11,15 @@ chrome.runtime.onMessage.addListener(
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
-        if (request.msg){
+        if (request.msg) {
             notifOptions.title = request.title;
             notifOptions.message = request.msg;
+            if (request.run && request.run >= 4 && request.run < 6)
+                notifOptions.iconUrl = '/img/icons/four_48.png';
+            else if (request.run && request.run >= 6)
+                notifOptions.iconUrl = '/img/icons/four_48.png';
+            else
+                notifOptions.iconUrl = '/img/icons/cricket_30.png';
             sendResponse({ farewell: "Notified" });
         }
         i++;
--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -25,14 +25,14 @@ $(function () {
             var newData = document.getElementsByClassName('cb-min-bat-rw')[1].firstElementChild.textContent.split(' ')[1].split('/');
             if (newData[0] != data[0]) {
                 var sRun = (newData[0] - data[0]) > 1 ? " Runs\n" : " Run\n";
-                chrome.runtime.sendMessage({ title: batsman + (newData[0] - data[0]) + sRun + team + " " + newData[0] + '/' + newData[1], msg: "Mach is being notified!" }, function (response) {
+                chrome.runtime.sendMessage({ title: batsman + (newData[0] - data[0]) + sRun + team + " " + newData[0] + '/' + newData[1], msg: "Mach is being notified!", run: newData[0] - data[0] }, function (response) {
                     console.log(response.farewell);
                 });
                 batsman = document.getElementsByClassName('cb-min-inf cb-col-100 ng-scope')[0].textContent.split('SR')[1].split('*')[0];
             }
             if (newData[1] != data[1]) {
                 var bowler = document.getElementsByClassName('cb-min-inf cb-col-100 ng-scope')[1].textContent.split('ECO')[1].split('*')[0];
-                chrome.runtime.sendMessage({ title: "OUT! by " + bowler + "\n" + team + " " + newData[0] + '/' + newData[1], msg: "Mach is being notified!" }, function (response) {
+                chrome.runtime.sendMessage({ title: "OUT! by " + bowler + "\n" + team + " " + newData[0] + '/' + newData[1], msg: "Mach is being notified!", wicket: newData[1] - data[1] }, function (response) {
                     console.log(response.farewell);
                 });
             }
