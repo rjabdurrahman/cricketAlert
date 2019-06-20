1eb8fd0e50f29ed5dcf672b6564a4adfdef50579
rjabdurrahman <md.rahman0296@gmail.com>
2019-06-20 14:16:46 +0600

    Changed: Notificaion just when needed

--- extension/background.js
+++ extension/background.js
@@ -1,22 +1,20 @@
 var notifOptions = {
     type: 'basic',
-    iconUrl: '/img/icons/icon_16.png',
-    title: 'Score',
-    message: 'Current Run is '
+    iconUrl: '/img/icons/cricket_30.png',
+    title: 'Open the Cricbuzz Match URL',
+    message: 'Notification Turned on...'
 }
-chrome.notifications.create('currentRun', notifOptions);
+chrome.notifications.create('installed', notifOptions);
 var i = 0;
-// setInterval(function(){
-//     i++;
-//     chrome.notifications.create('currentRun'+ i +'x', notifOptions);
-// }, 3000)
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
-        if (request.greeting == "hello")
-            sendResponse({ farewell: "goodbye" });
+        if (request.msg){
+            notifOptions.title = request.msg;
+            sendResponse({ farewell: "Notified" });
+        }
         i++;
         chrome.notifications.create('currentRun' + i + 'x', notifOptions);
     });
--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -1,16 +1,7 @@
-// $(function () {
-//     var el = document.getElementsByClassName('cb-min-bat-rw');
-//     var data = el[1].firstElementChild.textContent.split(' ')[1].split('/');
-//     console.log('Current Run is ' + data[0]);
-//     setInterval(function () {
-//         var newData = document.getElementsByClassName('cb-min-bat-rw')[1].firstElementChild.textContent.split(' ')[1].split('/');
-//         if (newData[0] != data[0])
-//             console.log('Current Run is ' + newData[0]);
-//             data[0] = newData[0];
-//     }, 3000)
-// });
-setInterval(function () {
-    chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
+var liveElements = document.getElementsByClassName('cb-min-bat-rw');
+console.log(liveElements[0]);
+if (liveElements.length == 0) {
+    chrome.runtime.sendMessage({ msg: "Match Yet not Started!" }, function (response) {
         console.log(response.farewell);
     });
-}, 3000);
+};
