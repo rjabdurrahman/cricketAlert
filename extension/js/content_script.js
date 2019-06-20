34e565319e0422f50569cd60a2fe194636e769d3
rjabdurrahman <md.rahman0296@gmail.com>
2019-06-20 17:03:11 +0600

    Added: Batting and Out status

--- extension/js/content_script.js
+++ extension/js/content_script.js
@@ -2,6 +2,7 @@ $(function () {
     var liveElements = document.getElementsByClassName('cb-min-bat-rw');
     var matchEnd = document.getElementsByClassName('cb-text-mom');
     var data = liveElements[1].firstElementChild.textContent.split(' ')[1].split('/');
+    var team = liveElements[1].firstElementChild.textContent.split(' ')[0];
     // if (matchEnd && !liveElements[0].textContent.includes('/')) {
     //     chrome.runtime.sendMessage({ title: matchEnd[0].textContent, msg: "Match Ended!" }, function (response) {
     //         console.log(response.farewell);
@@ -15,23 +16,22 @@ $(function () {
     }
     else {
         // Initial Notification Start
-        console.log('Current Run is ' + data[0]);
-        chrome.runtime.sendMessage({ title: "Score " + data[0] + '/' + data[1], msg: "Mach is being notified!" }, function (response) {
+        chrome.runtime.sendMessage({ title: team + " " + data[0] + '/' + data[1], msg: "Mach is being notified!" }, function (response) {
             console.log(response.farewell);
         });
         // Initial Notificaion End
         setInterval(function () {
             var newData = document.getElementsByClassName('cb-min-bat-rw')[1].firstElementChild.textContent.split(' ')[1].split('/');
             if (newData[0] != data[0]) {
-                console.log('Current Run is ' + newData[0]);
-                var sRun = (newData[0] - data[0]) > 1 ? " Runs": " Run";
-                chrome.runtime.sendMessage({ title: (newData[0] - data[0]) + sRun + " | Score " + newData[0] + '/' + newData[1], msg: "Mach is being notified!" }, function (response) {
+                var batsman = document.getElementsByClassName('cb-min-inf cb-col-100 ng-scope')[0].textContent.split('SR')[1].split('*')[0];
+                var sRun = (newData[0] - data[0]) > 1 ? " Runs | " : " Run | ";
+                chrome.runtime.sendMessage({ title: batsman + (newData[0] - data[0]) + sRun + team + " " + newData[0] + '/' + newData[1], msg: "Mach is being notified!" }, function (response) {
                     console.log(response.farewell);
                 });
             }
             if (newData[1] != data[1]) {
-                console.log('Current Run is ' + newData[0]);
-                chrome.runtime.sendMessage({ title: "OUT!" + " | Score" + newData[0] + '/' + newData[1], msg: "Mach is being notified!" }, function (response) {
+                var bowler = document.getElementsByClassName('cb-min-inf cb-col-100 ng-scope')[1].textContent.split('ECO')[1].split('*')[0];
+                chrome.runtime.sendMessage({ title: "OUT! by " + bowler + " | " + team + " " + newData[0] + '/' + newData[1], msg: "Mach is being notified!" }, function (response) {
                     console.log(response.farewell);
                 });
             }
